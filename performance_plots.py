66b607373fbb624ad770c8f6fc2bd572c72f86bb
reginacaputo <rcaputo@ucsc.edu>
2016-08-18 15:23:13 -0700

    fixing performance plot file

--- performance_plots.py
+++ performance_plots.py
@@ -2,17 +2,16 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 import numpy as np
 
+from astropy.io import ascii
 from operator import truediv
 
-## At some point should read in data files... 
-logEtrue_data = [2.5, 3., 3.5, 4., 4.5, 5.]
-Etrue_data = [316., 1000., 3160., 10000., 31600., 100000.]
-Ereco_data = [314., 995., 3143., 9392., 26140., 68190.]
-Eres_data = [12.24, 21.0, 48.0, 620.0, 3500., 9750.]
-Ares_data = [8.0, 2.5, 1.9, 16.75, 6.25, 2.75]
-Aeff_data = [25.4, 19.2, 7.2, 26.9, 133., 210.]
+## The data file in ecsv format...
+data = ascii.read('performance_plot_data.ecsv')
 
-print "This is how many data points you have:", len(Aeff_data)
+print "This is how many data points you have:", len(data)
+
+def column(matrix, i):
+    return [row[i] for row in matrix]
 
 def make_plt(xdata,ydata,points, xmin, xmax, ymin, ymax, xlabel='',ylabel='',log=False,loglog=False):
     plt.figure()
@@ -29,18 +28,26 @@ def make_plt(xdata,ydata,points, xmin, xmax, ymin, ymax, xlabel='',ylabel='',log
     plt.gca().set_xlabel(xlabel)
     plt.gca().set_ylabel(ylabel)
 
+#print column(data,'logEtrue_data')
 
-make_plt(logEtrue_data, Aeff_data,'ro-',2., 5.5, 1., 1000., r'log$_{10}$(Energy/keV)', r'A$_{\mathrm{eff}}$ [cm$^2$]', True, False)
+logEtrue = column(data,'logEtrue_data')
+Etrue = column(data,'Etrue_data')
+Erec = column(data,'Ereco_data')
+Eres = column(data,'Eres_data')
+Ares = column(data,'Ares_data')
+Aeff = column(data,'Aeff_data')
+
+make_plt(logEtrue, Aeff,'ro-',2., 5.5, 1., 1000., r'log$_{10}$(Energy/keV)', r'A$_{\mathrm{eff}}$ [cm$^2$]', True, False)
 plt.savefig('Aeff.png')
 plt.show()
 plt.close()
 
-make_plt(logEtrue_data, Ares_data,'go-',2., 5.5, 0., 20., r'log$_{10}$(Energy/keV)', r'A$_{\mathrm{res}}$ [deg]', False, False)
+make_plt(logEtrue, Ares,'go-',2., 5.5, 0., 20., r'log$_{10}$(Energy/keV)', r'A$_{\mathrm{res}}$ [deg]', False, False)
 plt.savefig('Ares.png')
 plt.show()
 plt.close()
 
-make_plt(logEtrue_data, map(truediv, Eres_data, Etrue_data),'bo-',2., 5.5, 0., 0.2, r'log$_{10}$(Energy/keV)', r'E$_{\mathrm{res}}$ [E$_width$/E$_true$]', False, False)
+make_plt(logEtrue, map(truediv, Eres, Etrue),'bo-',2., 5.5, 0., 0.2, r'log$_{10}$(Energy/keV)', r'E$_{\mathrm{res}}$ [E$_{\mathrm{width}}$/E$_{\mathrm{true}}$]', False, False)
 plt.savefig('Eres.png')
 plt.show()
 plt.close()
