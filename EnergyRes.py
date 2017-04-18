0b5540b172d5534a9423cc5174bfc38f561267fc
reginacaputo <rcaputo@ucsc.edu>
2017-04-18 11:09:02 -0400

    adding angular resoultion

--- EnergyRes.py
+++ EnergyRes.py
@@ -9,9 +9,10 @@ Date: April 14, 2017
 
 Usage Examples: Returns Energy smeared with a Gaussian
 import EnergyRes
-EnergyRes.Resolution(10.,0.5)
+EnergyRes.smear(10.,0.5)
 EnergyRes.ResolutionRange([5,10,25],[0.1,0.05,0.01])
-EnergyRes.ResolutionFromFile([5,10,25],filename="file.txt",doPlot=True)
+EnergyRes.EnergyResolutionFromFile([5,10,25],filename="file.txt",doPlot=True)
+EnergyRes.AngularResolutionFromFile([5.],[100.],filename="file.txt",doPlot=True)
 
 ------------------------------------------------------------------------
 """
@@ -22,33 +23,35 @@ import numpy as np
 from scipy.interpolate import interp1d
 from scipy import interpolate
 
-def Resolution(Energy, sigma):
+def smear(val, sigma):
 	"""
-	This is a function to smear a single given Energy by a gaussian with width sigma
+	This is a function to smear a single given value energy by a gaussian with width sigma
 	"""
-	s=np.random.normal(Energy, sigma, 1)
+	s=np.random.normal(val, sigma, 1)
 
 	return s[0]
 
-def ResolutionRange(Energy, sigma):
+
+def ResolutionRange(val, sigma):
 	"""
-	This is a function to smear an array of Energies by a gaussian with an array of widths
+	This is a function to smear an array of vals (energies or angles) by a gaussian with an array of widths
 	"""
 		
-	if(len(Energy) != len(sigma)):
+	if(len(val) != len(sigma)):
 		if len(sigma) !=1:
-			print "provide either one sigma for all E or sigmas for every E"
+			print "provide either one sigma for all values or sigmas for every value"
 			return
 
-	sEnergy=[]
+	sVals=[]
 
-	for i in range(len(Energy)):
+	for i in range(len(val)):
 		if len(sigma)==1:
-			sEnergy.append(Resolution(Energy[i],sigma[0]))
+			sVals.append(smear(val[i],sigma[0]))
 		else:
-			sEnergy.append(Resolution(Energy[i],sigma[i]))
+			sVals.append(smear(val[i],sigma[i]))
 
-	return sEnergy
+
+	return sVals
 
 
 def lineFun(x, a, b):
@@ -56,53 +59,146 @@ def lineFun(x, a, b):
 	return a*x+b
 
 
-def ResolutionFromFile(Energy, filename="EResTest.txt", doPlot=False, listResults=False):
+def AngularResolutionFromFile(input_energy=None, input_angle=None, energyanglefile=None, aresfile="AResTest.txt", doPlot=False, listResults=False): 
 	"""
-	This is a function that takes desired energies and interpolates the energy resolution 
-	from the inputs from a text file of Energy vs. sigmas
-	Energy is the energies that you would like the interpolated resolution for
-	filename is the list of energies vs. resolutions
+	This is a function that takes desired angles and energies and interpolates the angular resolution 
+	from the inputs from a text file of values (energy or angle) vs. sigmas
+	Val is the values that you would like the interpolated resolution for
+	filename is the list of energy vs. resolutions
 	there is a plot function that allows you to see how the interpolation went. 
 	Note: This is a simple linear interpolation... 
 	"""
 
-	file = open(filename, 'r')
+	energy=[]
+	angle=[]
+
+	if input_energy==None and input_angle==None:
+		energyangle=open(energyanglefile,'r')
+		for line in energyangle:
+			data=line.split()
+			energy.append(float(data[0]))
+			angle.append(float(data[1]))
+	elif energyanglefile==None:
+		if input_energy is not None and input_angle is not None:
+			energy=input_energy
+			angle=input_angle
+		else:
+			print "please enter energy and angle OR a file with that information"
+			return
+
+
+	file = open(aresfile, 'r')
 	x=[]
 	y=[]
+	interpRes=[]
 	tE=[]
 
 	for line in file:
 		vals=line.split()
-		x.append(float(vals[0]))
-		y.append(float(vals[1]))
+		if len(vals)==2:
+			x.append(float(vals[0]))
+			y.append(float(vals[1]))
+		else:
+			print "check file format"
+
+	#print x, y 
 
 	f = interp1d(x, y)
 	#f = interp1d(x, y, kind='cubic')
 	#f=interpolate.splrep(x, y, s=0)
 	xend=len(x)-1
-
-	interpRes=f(Energy)
+		
+	interpRes=f(energy)
 
 	if doPlot:
-		xnew=np.linspace(x[0],x[xend],num=41,endpoint=True)
-		plt.plot(x,y,'o',xnew,f(xnew),'--', Energy, interpRes, 'x')
+		#xnew=np.linspace(x[0],x[xend],num=41,endpoint=True)
+		plt.plot(x,y,'o', energy, interpRes, 'x')
 		plt.legend(['Measured Eres','Linear Interp', 'Interp. Eres'],loc='best')
 		plt.xscale('log')
 		plt.yscale('log')
 		plt.show()
 
-	smearedEnergies = ResolutionRange(Energy, interpRes)
+	smearedValues = ResolutionRange(angle, interpRes)
 
-	for j in range(len(smearedEnergies)):
-		tE.append(round(smearedEnergies[j],2))
+	for j in range(len(smearedValues)):
+		tE.append(round(smearedValues[j],2))
 
 	if listResults:
-		print "Energies: ", Energy
-		print "Smeared E: ", tE
+		print "angle: ", angle
+		print "Smeared Values: ", tE
+		print "Ares: ", interpRes
+
+
+	return smearedValues
+
+
+
+def EnergyResolutionFromFile(input_energy=None, energyfile=None, filename="EResTest.txt", doPlot=False, listResults=False):
+	"""
+	This is a function that takes desired energies and interpolates the energy resolution 
+	from the inputs from a text file of values (energy or angle) vs. sigmas
+	Val is the values that you would like the interpolated resolution for
+	filename is the list of energy vs. resolutions
+	there is a plot function that allows you to see how the interpolation went. 
+	Note: This is a simple linear interpolation... 
+	"""
+
+	if input_energy==None:
+		f_energy=open(energyfile,'r')
+		for line in f_energy:
+			data=line.split()
+			energy.append(float(data[0]))
+	elif energyfile==None:
+		if input_energy is not None:
+			energy=input_energy
+		else:
+			print "please enter energy and angle OR a file with that information"
+			return
+
+
+	file = open(filename, 'r')
+	x=[]
+	y=[]
+	interpRes=[]
+	tE=[]
+
+	for line in file:
+		vals=line.split()
+		if len(vals)==2:
+			x.append(float(vals[0]))
+			y.append(float(vals[1]))
+		else:
+			print "check file format"
+
+
+	f = interp1d(x, y)
+	#f = interp1d(x, y, kind='cubic')
+	#f=interpolate.splrep(x, y, s=0)
+	xend=len(x)-1
+		
+	interpRes=f(energy)
+
+	if doPlot:
+		xnew=np.linspace(x[0],x[xend],num=41,endpoint=True)
+		plt.plot(x,y,'o',xnew,f(xnew),'--', energy, interpRes, 'x')
+		plt.legend(['Measured Eres','Linear Interp', 'Interp. Eres'],loc='best')
+		plt.xscale('log')
+		plt.yscale('log')
+		plt.show()
+
+	smearedValues = ResolutionRange(energy, interpRes)
+
+	for j in range(len(smearedValues)):
+		tE.append(round(smearedValues[j],2))
+
+	if listResults:
+		print "Energies: ", energy
+		print "Smeared Values: ", tE
 		print "Eres: ", interpRes
 
 
-	return smearedEnergies
+	return smearedValues
+
 
 
 
