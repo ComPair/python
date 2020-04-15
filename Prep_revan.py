e7ed73e65746e59ab92c81a0ae209cccb9e099d3
MitteDesBerges <37981558+MitteDesBerges@users.noreply.github.com>
2020-04-15 18:05:12 +0200

    modified for gziped sims and mcosima sims

--- Prep_revan.py
+++ Prep_revan.py
@@ -5,14 +5,25 @@
 # Before using this code, check geofile, Log_E, and angles parameters.
 # This code will generate runRevan.sh file.
 
+# Jan
+# Date April 15th, 2020
+# Modifications: 
+# added support for gziped simulations, BEWARE: the output .tra is also going to be gziped. ToDo: either modifiy the analysis pipeline to cope with gzips or extract the .tra file before performing the analysis
+# when processing mcosima sims: if you used the prep_mcosima script run runRevan.sh in the parent directory of data/ (where the simulation parts are stored). Also make sure that the concatenation files are stored in the parent directory, not in the data/ directory. 
+# This way the script can only perform reconstructions on the whole set and not on each part.
+# New globals:
+# boolean: compressed, whether the sim is gziped or not
+# boolean: isMcosima, whether the sims were done with mcosima 
+
 import numpy as np
 import math
 import argparse
 import os
 
-Log_E=[2.2,2.5,2.7,3,3.2,3.5,3.7,4,4.2,4.5,4.7,5,5.2,5.5,5.7,6,6.2,6.5,6.7]
+#Log_E=[2.2,2.5,2.7,3,3.2,3.5,3.7,4,4.2,4.5,4.7,5,5.2,5.5,5.7,6,6.2,6.5,6.7]
+Log_E=[2.2,2.5,2.7,3,3.2,3.5,3.7,4,4.2,4.5,4.7,5,5.2,5.5,5.7,6]
 #angles  =[0,25.8,36.9,45.6,53.1,60]
-angles = [0, 36.9]
+angles = [0]
 OneBeam= 'FarFieldPointSource'
 geometry_file='AmegoBase.geo.setup'
 
@@ -61,9 +72,21 @@ if __name__ == "__main__":
     base_file= args.b
 
     args = parser.parse_args()
+    compressed = True
+    isMcosima = True
+
     with open("./runRevan.sh", mode='w') as f:
         for myene in energies:
             for cosTh,ang in zip(cos_ang,angles):
                 source_file='%s_%.3fMeV_Cos%.1f'%(OneBeam,myene/1000.,cosTh)
                 create_file(source_file)
-                f.write("revan -a -n -f {}.inc1.id1.sim -g {}/{} -c {}.cfg\n".format(source_file, geometry_dir,geometry_file, source_file))
+                if compressed == True:
+                    if isMcosima == False:
+                        f.write("revan -a -n -f {}.inc1.id1.sim.gz -g {}/{} -c {}.cfg\n".format(source_file, geometry_dir,geometry_file, source_file))
+                    else:
+                        f.write("revan -a -n -f {}.p1.sim.gz -g {}/{} -c {}.cfg\n".format(source_file, geometry_dir,geometry_file, source_file))
+                else:
+                    if isMcosima == False:
+                        f.write("revan -a -n -f {}.inc1.id1.sim -g {}/{} -c {}.cfg\n".format(source_file, geometry_dir,geometry_file, source_file))
+                    else:
+                        f.write("revan -a -n -f {}.p1.sim -g {}/{} -c {}.cfg\n".format(source_file, geometry_dir,geometry_file, source_file))
